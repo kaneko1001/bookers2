c09497b3f70d57592334f4f620a7c2cfef5a6ad2
HikaruKaneko <127648417+kaneko1001@users.noreply.github.com>
2023-04-20 14:14:31 +0000

    [課題3]RSpec完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
     @books = Book.all
@@ -11,6 +12,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @user = current_user
@@ -33,6 +35,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
         render :edit
@@ -51,4 +54,10 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,29 @@
 class UsersController < ApplicationController
+   before_action :is_matching_login_user, only: [:edit, :update]
 
   def edit
     @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def index
     @users = User.all
     @books = Book.all
+    @book = Book.new
     @user = current_user
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books.all
+    @book = Book.new
   end
 
   def update
+    @book = Book.new
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
     else
       render :edit
@@ -28,5 +34,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image , :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 end
